3c5f11cc228f552f57507dcc19174cfd4a38efd9
Alejandro Peña Restrepo <alejoperes2003@gmail.com>
2024-10-30 01:12:40 -0500

    segundo commit

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -3,6 +3,7 @@
 # validador.py
 from abc import ABC, abstractmethod
 
+# validador.py
 class ReglaValidacion(ABC):
     def __init__(self, longitud_esperada):
         self._longitud_esperada = longitud_esperada
@@ -10,6 +11,15 @@ class ReglaValidacion(ABC):
     def _validar_longitud(self, clave):
         if len(clave) <= self._longitud_esperada:
             raise ValueError(f"La clave debe tener más de {self._longitud_esperada} caracteres.")
+
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
+
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
 
     @abstractmethod
     def es_valida(self, clave):
